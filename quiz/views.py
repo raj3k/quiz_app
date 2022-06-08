8fc88ce0651cbdc9c5c4968e99e12350c0ae8613
raj3k <rajkosky@gmail.com>
2022-06-08 12:21:04 +0200

    game logic

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from quiz.game.quiz import Quiz
+from .api import ApiClient
+
+
+def index(request):
+    try:
+        quiz = ApiClient.get_quiz_options()
+        return render(request, "quiz/index.html", quiz)
+    except ValueError:
+        return render(request, "quiz/error.html")
+
+
+def start_game(request):
+    number_of_questions = request.POST['quantity']
+    difficulty = request.POST['difficulty']
+    category = request.POST['category']
+    quiz = Quiz.create_game(number_of_questions, difficulty, category)
+    quiz.save(request)
+    return redirect('/on_game')
+
+
+def on_game(request):
+    quiz = Quiz.restore(request)
+    if not quiz:
+        return render(request, 'quiz/error.html')
+
+    answer = request.POST.get('answer')
+    if answer:
+        quiz.check_answer(answer)
+
+    try:
+        question = quiz.get_question()
+        quiz.save(request)
+        return render(request, 'quiz/game.html', vars(question))
+    except IndexError as e:
+        return redirect('/finish')
+
+
+def finish(request):
+    import pdb
+    pdb.set_trace()
+    quiz = Quiz.restore(request)
+    result = quiz.get_result()
+    quiz.stop(request)
+    return render(request, 'quiz/finish.html', result)
+
