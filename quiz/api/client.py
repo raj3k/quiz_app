474454019ca0f14a904a47ab9157f4e4af5986ff
raj3k <rajkosky@gmail.com>
2022-06-17 15:17:09 +0200

    docstrings and comments

--- quiz/api/client.py
+++ quiz/api/client.py
@@ -14,12 +14,18 @@ class ApiClient:
 
     @classmethod
     def get_quiz_options(cls):
+        '''
+        Returns dictionary with options to select for user.
+        '''
         result = {"categories": requests.get(ApiClient.CATEGORIES_URL).json()["trivia_categories"], 
                 "max_questions": MAX_QUESTIONS, "difficulty": DIFFICULTY}
         return result
 
     @classmethod
     def get_questions(cls, number_of_questions, category, difficulty):
+        '''
+        Returns dictionary with questions based on provided options by user from OpenTriviaDB.
+        '''
         total_question_count, total_easy_question_count, total_medium_question_count, total_hard_question_count = itemgetter(
             'total_question_count',
             'total_easy_question_count',
@@ -28,10 +34,14 @@ class ApiClient:
         )(requests.get(ApiClient.QUESTIONS_COUNT_FOR_CATEGORY.format(category)).json()['category_question_count'])
 
         if int(number_of_questions) >= total_question_count:
+            # if requested number of questions is higher than total count of questions in API then returns total available
+            # questions from API
             results = requests.get(ApiClient.QUESTIONS_URL_WITHOUT_DIFFICULTY.format(total_question_count, category)).json()['results']
         else:
             results = requests.get(ApiClient.QUESTIONS_URL_WITH_DIFFICULTY.format(number_of_questions, category, difficulty)).json()['results']
             if not results:
+                # If requested number of questions for a given category and difficulty is higher than total count for that category and difficulty
+                # then load all available questions for that category with a requested category
                 if difficulty == "easy":
                     results = requests.get(ApiClient.QUESTIONS_URL_WITH_DIFFICULTY.format(total_easy_question_count, category, difficulty)).json()['results']
                 elif difficulty == "medium":
